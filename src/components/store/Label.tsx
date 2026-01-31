845c837d0668d0538d1f2564d6ad62a6ccdcad1f
yusing <yusing.wys@gmail.com>
2026-01-31 18:02:58 +0800

    feat(store): add className prop to StoreLabel component
    Allow custom styling on Label components by accepting an optional className
    prop that merges with default layout classes via cn() utility. Also refined
    tooltip styling by removing manual margins and adding min-w-fit to content.

--- src/components/store/Label.tsx
+++ src/components/store/Label.tsx
@@ -2,6 +2,7 @@
 
 import { FieldLabel } from '@/components/ui/field'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { cn } from '@/lib/utils'
 import { IconInfoCircle } from '@tabler/icons-react'
 import { useIdTitle } from './hooks'
 import type { FormComponentProps, Prettify, StoreFieldPropsCommon } from './types'
@@ -20,19 +21,24 @@ function StoreLabel<T, Form = false>({
   title,
   description,
   descriptionVariant = 'inline',
+  className,
   ...labelProps
 }: LabelProps<T, Form>) {
   const { fieldId, fieldTitle } = useIdTitle({ state, id, title })
 
   return (
-    <FieldLabel htmlFor={fieldId} {...labelProps}>
+    <FieldLabel
+      htmlFor={fieldId}
+      className={cn('flex justify-between gap-2', className)}
+      {...labelProps}
+    >
       {fieldTitle}
       {descriptionVariant === 'tooltip' && description && (
         <Tooltip>
           <TooltipTrigger>
-            <IconInfoCircle className="size-4 m-0.5 ml-auto" />
+            <IconInfoCircle className="size-4" />
           </TooltipTrigger>
-          <TooltipContent>{description}</TooltipContent>
+          <TooltipContent className="min-w-fit">{description}</TooltipContent>
         </Tooltip>
       )}
     </FieldLabel>
